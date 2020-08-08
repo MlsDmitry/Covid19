212b1fc2badf5523951675941aa9921470a854e5
MlsDmitry <43723140+MlsDmitry@users.noreply.github.com>
2020-08-08 06:06:11 +0300

    npm repackage

--- light-wave-front/template/regestration/regbtm.php
+++ light-wave-front/template/regestration/regbtm.php
@@ -1,4 +1,19 @@
 <?php
-$url = $_SERVER['REQUEST_URI'];
-echo $url;
+$url = 'http://www.example.org/api/login';
+
+$context = stream_context_create(array(
+    'http' => array(
+        'method' => 'POST',
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query(
+            array(
+                'username' => 'mlsdmitry',
+                'password' => 'Vjkjrjdbx',
+                )
+            ),
+            'timeout' => 60
+    )
+));
+
+$resp = file_get_contents($url, FALSE, $context);
 ?>
